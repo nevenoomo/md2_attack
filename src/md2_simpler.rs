dcd91f58232a61963fe3afe043d244465b9e6c23
Neve Noomo <d3ad.l1n3@yandex.ru>
2019-09-08 19:26:24 +0300

    Fixed bug, tests pass

--- src/md2_simpler.rs
+++ src/md2_simpler.rs
@@ -9,13 +9,9 @@ static S: [u8; 4] = [1, 3, 0, 2];
 
 /// Calculates a MD2 digest for a given message
 pub fn digest(mut m: Vec<u8>) -> [u8; 16] {
-    let mut digest_buffer = [0u8; 48];
-
     stages::padd(&mut m); // add padding to the message
     stages::add_checksum(&mut m); // add checksum to the message
-    stages::process_message(&m, &mut digest_buffer); // process message in blocks
-    let mut digest: [u8; 16] = [0u8; 16];
-    digest.copy_from_slice(&digest_buffer[..16]);
+    let digest = m.chunks(16).fold([0u8; 16], |s, b| stages::process_block(&s, b));
 
     digest
 }
@@ -27,10 +23,10 @@ mod stages {
         // padd the message M with i bytes of value i mod 4 to make N = len(M)
         // to be a multiple of 16
         m.append(
-            &mut iter::repeat(m.len() as u8 % 4)
+            &mut iter::repeat(m.len() as u8 & 3u8)
                 .take(16 - m.len() % 16)
                 .collect(),
-        ); // IDEA try &mut &[m.len() as u8 % 4].repeat(16 - m.len() % 16)
+        );
     }
 
     pub fn add_checksum(m: &mut Vec<u8>) {
@@ -41,7 +37,7 @@ mod stages {
         for i in 0..m.len() / 16 {
             for j in 0..16 {
                 let c = m[i * 16 + j] as usize;
-                checksum[j] = super::S[c ^ l];
+                checksum[j] ^= super::S[c ^ l];
                 l = checksum[j] as usize;
             }
         }
@@ -49,31 +45,40 @@ mod stages {
         m.extend_from_slice(&checksum);
     }
 
-    pub fn process_message(m: &Vec<u8>, buf: &mut [u8; 48]) {
-        for i in 0..m.len() / 16 {
-            for j in 0..16 {
-                buf[16 + j] = m[i * 16 + j];
-                buf[32 + j] = buf[16 + j] ^ buf[j];
-            }
-
-            let mut t = 0u8;
-
-            for j in 0..18 {
-                for k in 0..48 {
-                    t = buf[k] ^ super::S[t as usize];
-                    buf[k] = t;
-                }
-
-                t = t.overflowing_add(j).0 & 3u8; // take least two bits
-            }
+    pub fn process_block(state: &[u8], block: &[u8]) -> [u8; 16]{
+        if block.len() != 16 && state.len() != 16 {
+            panic!("Incorrect block size.");
         }
-    }
+
+        let mut buf = [0u8; 48];
+        
+        for i in 0..16 {
+            buf[i] = state[i];
+            buf[i + 16] = block[i];
+            buf[i + 32] = buf[i+16]^buf[i];
+        }
+
+        let mut t: u8 = 0;
+        for i in 0..18 {
+            for j in 0..48 {
+                buf[j] ^= super::S[t as usize];
+                t = buf[j as usize];
+            }
+
+            t = t.wrapping_add(i) & 0x3;
+        }
+
+        let mut result = [0u8; 16];
+        result.copy_from_slice(&buf[..16]);
+
+        result
+    }    
 }
 
 #[cfg(test)]
 mod test {
     #[test]
-    fn digest() {
+    fn simple_digest() {
         let message = vec![
             0u8, 3u8, 1u8, 2u8, 1u8, 1u8, 0u8, 2u8, 0u8, 3u8, 3u8, 0u8, 1u8, 1u8, 2u8, 0u8, 3u8,
             2u8, 2u8, 2u8, 2u8, 2u8, 1u8, 0u8, 0u8, 3u8, 3u8, 0u8, 1u8, 2u8, 3u8, 0u8,
